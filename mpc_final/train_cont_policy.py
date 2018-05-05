d23793a87fe3fbe897eab7e2114f12988825130f
xinleipan <xinleipan@berkeley.edu>
2018-05-05 01:08:10 -0700

    update torcs wrapper

--- mpc_final/train_cont_policy.py
+++ mpc_final/train_cont_policy.py
@@ -7,41 +7,6 @@ import copy
 import cv2
 from utils import *
 
-class TorcsWrapper:
-    def __init__(self, env, imsize=(256, 256)):
-        self.env = env
-        self.imsize = imsize
-        self.doneCond = DoneCondition(30)
-        self.epi_len = 0
-
-    def reset(self):
-        obs = self.env.reset()
-        self.doneCond = DoneCondition(30)
-        self.epi_len = 0
-        return cv2.resize(obs, self.imsize)
-         
-    def step(self, action):
-        self.epi_len += 1
-        obs, reward, real_done, info = self.env.step(action)
-        dist_this = info['speed']*(np.cos(info['angle'])-np.abs(np.sin(info['angle']))-np.abs(info['trackPos'])/9.0)
-        reward_with_pos = info['speed']*(np.cos(info['angle'])-np.abs(np.sin(info['angle']))-np.abs(info['trackPos'])/9.0)/40.0
-        reward_without_pos = info['speed']*(np.cos(info['angle'])-np.abs(np.sin(info['angle'])))/40.0
-        done = self.doneCond.isdone(info['trackPos'], dist_this, info['pos']) or self.epi_len > 1000
-        
-        off_flag = int(info['trackPos']>=3 or info['trackPos']<=-1)
-        coll_flag = int(abs(info['trackPos'])>7)
-        obs = cv2.resize(obs, self.imsize)
-        reward = {}
-        reward['with_pos'] = reward_with_pos
-        reward['without_pos'] = reward_without_pos
-        info['off_flag'] = off_flag
-        info['coll_flag'] = coll_flag
-
-        return obs, reward, done, info
-
-    def close(self):
-        self.env.close()
-
 class DQNAgent:
     def __init__(self, frame_history_len=4, num_actions=11, lr=0.0001, exploration=None, save_path=None):
         self.dqn_net = atari_model(3 * frame_history_len, num_actions, frame_history_len).cuda().float().train()
