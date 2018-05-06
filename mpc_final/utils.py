7b312e937796c952b895b1cf0e87d8f410e05890
Xiangyu Chen <1023764859@qq.com>
2018-05-06 23:00:48 +0800

    debug

--- mpc_final/utils.py
+++ mpc_final/utils.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function
 import torch
 import torch.nn as nn
+from torch import optim
 from torch.autograd import Variable
 import numpy as np
 import pickle as pkl
