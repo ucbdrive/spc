b24da9464574fedcbcc815b01a51347e0652b524
xinleipan <xinleipan@berkeley.edu>
2018-05-13 16:49:03 -0700

    Delete test_model.py

--- mpc_final/test_model.py
+++
@@ -1,7 +0,0 @@
-from model import *
-
-net = ConvLSTMMulti(3, True, 4, True, True, 'drn_d_22', 4, 1024, 64)
-imgs = Variable(torch.randn(1,15, 12, 256, 256))
-act = Variable(torch.randn(1, 15, 3))
-y = net(imgs, act, 15)
-pdb.set_trace()
