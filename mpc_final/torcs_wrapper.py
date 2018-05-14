62a3ee813d9055b67cd18330403c7bfe62da2f0b
xinleipan <xinleipan@berkeley.edu>
2018-05-13 17:52:40 -0700

    at least 500 steps is required to random reset

--- mpc_final/torcs_wrapper.py
+++ mpc_final/torcs_wrapper.py
@@ -24,7 +24,7 @@ class TorcsWrapper:
     def reset(self):
         obs = self.env.reset()
         if self.random_reset:
-            rand_step = np.random.randint(200)
+            rand_step = np.random.randint(500)
             for i in range(rand_step):
                 obs, _, _, _ = self.env.step(naive_driver(self.env.get_info(), self.continuous))
         self.doneCond = DoneCondition(30)
