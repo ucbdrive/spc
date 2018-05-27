340075e1a598b26bc2feb33f54b23c498d057624
harrypann <xinleipan@berkeley.edu>
2018-05-27 14:52:44 -0700

    update train continuous policy

--- mpc_final/train_cont_policy.py
+++ mpc_final/train_cont_policy.py
@@ -41,7 +41,142 @@ def init_models(args):
             dqn_agent.load_model()
     else:
         dqn_agent = None 
-    return train_net, net, optimizer, epoch, exploration, dqn_agent
+
+    if args.resume:
+        try:
+            num_imgs_start = max(int(open(args.save_path + '/log_train_torcs.txt').readlines()[-1].split(' ')[1]) - 1000,0)
+        except:
+            print('cannot find file, num_imgs_start is 0')
+            num_imgs_start = 0
+    else:
+        num_imgs_start = 0
+
+    return train_net, net, optimizer, epoch, exploration, dqn_agent, num_imgs_start
+
+class BufferManager:
+    def __init__(self, args=None):
+        self.args = args
+        self.mpc_buffer = MPCBuffer(args)
+        self.img_buffer = IMGBuffer(1000)
+        self.obs_buffer = ObsBuffer(args.frame_history_len)
+        self.epi_rewards = []
+        self.rewards = 0.0
+        self.prev_act = np.array([1.0, 0.0]) if args.continuous else 1
+        
+        self.prev_info = None
+        self.avg_img = None
+        self.std_img = None
+        self.avg_img_t = None
+        self.std_img_t = None
+        self.speed_np = None
+        self.pos_np = None
+        self.posxyz_np = None
+        self.prev_xyz = None
+        self.epi_rewards_with = []
+        self.epi_rewards_without = []
+        self.rewards_with = 0.0
+        self.rewards_without = 0.0
+        self.mpc_ret = 0
+        
+    def step_first(self, obs, info):
+        self.img_buffer.store_frame(obs)
+        self.prev_info = info
+        self.avg_img, self.std_img, self.avg_img_t, self.std_img_t = self.img_buffer.get_avg_std(gpu=0)
+        self.speed_np, self.pos_np, self.posxyz_np = get_info_np(info, use_pos_class=False)
+        self.prev_xyz = np.array(info['pos'])
+
+    def store_frame(self, obs):
+        self.mpc_ret = self.mpc_buffer.store_frame(obs)
+        this_obs_np = self.obs_buffer.store_frame(obs, self.avg_img, self.std_img)
+        obs_var = Variable(torch.from_numpy(this_obs_np).unsqueeze(0)).float.cuda()
+        self.img_buffer.store_frame(obs)
+        return self.mpc_ret, obs_var
+    
+    def store_effect(self, action, reward, done, info, seg):
+        self.prev_act = action
+        self.speed_np, self.pos_np, self.posxyz_np = get_info_np(info, use_pos_class = False)
+        offroad_flag, coll_flag = info['off_flag'], info['coll_flag']
+        speed_list, pos_list = get_info_ls(self.prev_info)
+        if self.args.use_xyz:
+            xyz = np.array(info['pos'])
+            rela_xyz = xyz - self.prev_xyz
+            self.prev_xyz = xyz
+        else:
+            rela_xyz = None
+        self.mpc_buffer.store_effect(self.mpc_ret, action, done, coll_flag, offroad_flag, info['speed'], \
+                    info['angle'], pos_list[0], rela_xyz, seg)
+        self.rewards_with += reward['with_pos']
+        self.rewards_without += reward['without_pos']
+
+    def store_info(self, info):
+        self.prev_info = info
+
+    def update_avg_std_img(self):
+        self.avg_img, self.std_img, self.avg_img_t, self.std_img_t = self.img_buffer.get_avg_std()
+    
+    def reset(self, info, step):
+        self.obs_buffer.clear()
+        self.epi_rewards_with.append(self.rewards_with)
+        self.epi_rewards_without.append(self.rewards_without)
+        self.rewards_with, self.rewards_without = 0, 0
+        self.prev_act = np.array([1.0, 0.0]) if self.args.continuous else 1
+        self.speed_np, self.pos_np, self.posxyz_np = get_info_np(info, use_pos_class = False)
+        print('past 100 episode rewards is', \
+            "{0:.3f}".format(np.mean(self.epi_rewards_with[-100:])), \
+                ' std is ', "{0:.15f}".format(np.std(self.epi_rewards_with[-100:])))
+        with open(self.args.save_path+'/log_train_torcs.txt', 'a') as fi:
+            fi.write('step '+str(step))
+            fi.write(' reward_with ' + str(np.mean(self.epi_rewards_with[-10:])))
+            fi.write(' std ' + str(np.std(self.epi_rewards_with[-10:])))
+            fi.write(' reward_without ' + str(np.mean(self.epi_rewards_without[-10:])))
+            fi.write(' std ' + str(np.std(self.epi_rewards_without[-10:])) + '\n')      
+
+class ActionSampleManager:
+    def __init__(self, args):
+        self.args = args
+        self.prev_act = np.array([1.0, 0.0]) if self.args.continuous else 1
+        if self.args.use_dqn:
+            self.prev_dqn_act = 0
+        else:
+            self.prev_dqn_act = None
+
+    def sample_action(self, net, dqn_net, obs, obs_var, exploration, tt):
+        if tt % self.args.num_same_step != 0:
+            return self.process_act(self.prev_act, self.prev_dqn_act)
+        else:
+            if self.args.continuous:
+                if random.random() <= 1 - exploration.value(tt):
+                    action = sample_cont_action(self.args, net, obs_var, prev_action=self.prev_act)
+                else:
+                    action = np.random.rand(self.args.num_total_act)*2-1
+                action = np.clip(action, -1, 1)
+                if self.args.use_dqn:
+                    dqn_act = dqn_net.sample_action(obs, tt)
+                else:
+                    dqn_act = None
+            else:
+                if random.random() <= 1- exploration.value(tt):
+                    action = sample_discrete_action(self.args, net, obs_var, prev_action=self.prev_act)
+                else:
+                    action = np.random.randint(self.args.num_total_act)
+                dqn_act = None
+            action, dqn_act = self.process_act(action, dqn_act)
+            self.prev_act = action
+            self.prev_dqn_act = dqn_act
+            return action, dqn_act               
+
+    def reset(self):
+        self.prev_act = np.array([1.0, 0.0]) if self.args.continuous else 1
+        if self.args.use_dqn:
+            self.prev_dqn_act = 0
+        else:
+            self.prev_dqn_act = None
+ 
+    def process_act(self, act, dqn_act):
+        if self.args.use_dqn and self.args.continuous:
+            if abs(act[1]) <= dqn_act * 0.1:
+                act[1] = 0
+        return act, dqn_act        
 
 def train_policy(args, env, num_steps=40000000):
     ''' basics '''
@@ -53,101 +188,39 @@ def train_policy(args, env, num_steps=40000000):
         os.remove(os.path.join(args.save_path, 'distlog.txt'))
 
     ''' create model '''
-    train_net, net, optimizer, epoch, exploration, dqn_agent = init_models(args)
+    train_net, net, optimizer, epoch, exploration, dqn_agent, num_imgs_start = init_models(args)
     
     ''' load buffers '''
-    mpc_buffer = MPCBuffer(args)
-    img_buffer = IMGBuffer(1000)
-    obs_buffer = ObsBuffer(args.frame_history_len)
-    
+    buffer_manager = BufferManager(args)
+    action_manager = ActionSampleManager(args)
+
     done_cnt = 0
-    epi_rewards, rewards = [], 0.0
     _ = env.reset()
-    prev_act = np.array([1.0, 0.0]) if args.continuous else 1
-    obs, reward, done, info = env.step(prev_act)
-    img_buffer.store_frame(obs)
-    prev_info = copy.deepcopy(info)
-    avg_img, std_img, avg_img_t, std_img_t = img_buffer.get_avg_std(gpu = 0)
-    speed_np, pos_np, posxyz_np = get_info_np(info, use_pos_class = False)
-    prev_xyz = np.array(info['pos'])
-
-    if args.resume:
-        try:
-            num_imgs_start = max(int(open(args.save_path + '/log_train_torcs.txt').readlines()[-1].split(' ')[1]) - 1000,0)
-        except:
-            print('cannot find file, num_imgs_start is 0')
-            num_imgs_start = 0
-    else:
-        num_imgs_start = 0
-
-    epi_rewards_with, epi_rewards_without = [], []
-    rewards_with, rewards_without = 0, 0
-    start_testing = False
+    obs, reward, done, info = env.step(buffer_manager.prev_act)
+    buffer_manager.step_first(obs, info)
     done_cnt = 0
     for tt in range(num_imgs_start, num_steps):
-        if args.use_dqn:
-            dqn_action = dqn_agent.sample_action(obs, tt)
-        ret = mpc_buffer.store_frame(obs)
-        this_obs_np = obs_buffer.store_frame(obs, avg_img, std_img)
-        obs_var = Variable(torch.from_numpy(this_obs_np).unsqueeze(0)).float().cuda()
+        ret, obs_var = buffer_manager.store_frame(obs)
 
-        if tt % args.num_same_step != 0:
-            action = prev_act
-            real_action = action
-            if args.continuous:
-                real_action[0] = real_action[0] * 0.5 + 0.5        
-        elif args.continuous:
-            if random.random() <= 1 - exploration.value(tt):
-                ## todo: finish sample continuous action function
-                action = sample_cont_action(args, net, obs_var, prev_action = prev_act)
-            else:
-                action = np.random.rand(args.num_total_act) * 2 - 1
-            action = np.clip(action, -1, 1)
-
-            if args.use_dqn:
-                if abs(action[1]) <= dqn_action * 0.1:
-                    action[1] = 0
-            real_action = action
-            real_action[0] = real_action[0] * 0.5 + 0.5
-        else:
-            if random.random() <= 1 - exploration.value(tt):
-                real_action = sample_discrete_action(args, net, obs_var, prev_action = prev_act)
-            else:
-                real_action = np.random.randint(args.num_total_act)
-            action = real_action
-
-        obs, reward, done, info = env.step(real_action)
+        action, dqn_action = action_manager.sample_action(net, dqn_agent, obs, obs_var, exploration, tt)
+        obs, reward, done, info = env.step(action)
         if args.target_speed > 0:
             with open(os.path.join(args.save_path, 'speedlog.txt'), 'a') as f:
                 f.write('step %d speed %0.4f\n' % (tt, info['speed']))
         if args.target_dist > 0:
             with open(os.path.join(args.save_path, 'distlog.txt'), 'a') as f:
                 f.write('step %d dist %0.4f\n' % (tt, info['speed'] * (np.cos(info['angle']) - np.abs(np.sin(info['angle'])))))
-        img_buffer.store_frame(obs)
         if args.continuous:
             print('action', "{0:.2f}".format(action[0]), "{0:.2f}".format(action[1]), ' pos ', "{0:.2f}".format(info['trackPos']), "{0:.2f}".format(info['pos'][0]), "{0:.2f}".format(info['pos'][1]),\
                 ' angle ', "{0:.2f}".format(info['angle']), ' reward ', "{0:.2f}".format(reward['with_pos']), ' explore ', "{0:.2f}".format(exploration.value(tt)))
         else:
             print('action', '%d' % real_action, ' pos ', "{0:.2f}".format(info['trackPos']), "{0:.2f}".format(info['pos'][0]), "{0:.2f}".format(info['pos'][1]),\
                 ' angle ', "{0:.2f}".format(info['angle']), ' reward ', "{0:.2f}".format(reward['with_pos']), ' explore ', "{0:.2f}".format(exploration.value(tt)))
-        prev_act = action
-        speed_np, pos_np, posxyz_np = get_info_np(info, use_pos_class = False)
-        offroad_flag, coll_flag = info['off_flag'], info['coll_flag']
-        speed_list, pos_list = get_info_ls(prev_info)
-        if args.use_xyz:
-            xyz = np.array(info['pos'])
-            rela_xyz = xyz - prev_xyz
-            prev_xyz = xyz
-        else:
-            rela_xyz = None
-
         seg = env.env.get_segmentation().reshape((1, 256, 256)) if args.use_seg else None
-        mpc_buffer.store_effect(ret, action, done, coll_flag, offroad_flag, info['speed'], info['angle'], pos_list[0], rela_xyz, seg)
-        rewards_with += reward['with_pos']
-        rewards_without += reward['without_pos']
+        buffer_manager.store_effect(action, reward, done, info, seg)
 
         if tt % 100 == 0:
-            avg_img, std_img, avg_img_t, std_img_t = img_buffer.get_avg_std()
+            buffer_manager.update_avg_std_img()
 
         if done:
             done_cnt += 1
@@ -155,30 +228,16 @@ def train_policy(args, env, num_steps=40000000):
                 test_reward = test(args, env, net, avg_img, std_img)
                 with open(os.path.join(args.save_path, 'test_log.txt'), 'a') as f:
                     f.write('step %d reward_with %f reward_without %f\n' % (tt, test_reward['with_pos'], test_reward['without_pos']))
-            obs_buffer.clear()
-            epi_rewards_with.append(rewards_with)
-            epi_rewards_without.append(rewards_without)
             obs = env.reset()
-            rewards_with, rewards_without = 0, 0
-            prev_act = np.array([1.0, 0.0]) if args.continuous else 1
             obs, reward, done, info = env.step(prev_act)
-            speed_np, pos_np, posxyz_np = get_info_np(info, use_pos_class = False)
-            print('past 100 episode rewards is ', \
-                "{0:.3f}".format(np.mean(epi_rewards_with[-100:])), \
-                ' std is ', "{0:.15f}".format(np.std(epi_rewards_with[-100:])))
-            with open(args.save_path+'/log_train_torcs.txt', 'a') as fi:
-                fi.write('step ' + str(tt))
-                fi.write(' reward_with ' + str(np.mean(epi_rewards_with[-10:])))
-                fi.write(' std ' + str(np.std(epi_rewards_with[-10:])))
-                fi.write(' reward_without ' + str(np.mean(epi_rewards_without[-10:])))
-                fi.write(' std ' + str(np.std(epi_rewards_without[-10:])) + '\n')
+            buffer_manager.reset(info, tt)
+            action_manager.reset()
+        buffer_manager.store_info(info)
         
-        prev_info = copy.deepcopy(info) 
         if args.use_dqn:
             dqn_agent.store_effect(dqn_action, reward['with_pos'], done)
         
         if tt % args.learning_freq == 0 and tt > args.learning_starts and mpc_buffer.can_sample(args.batch_size):
-            start_testing = True
             for ep in range(50):
                 optimizer.zero_grad()
                 
