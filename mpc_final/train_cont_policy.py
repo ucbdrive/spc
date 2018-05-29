9a890325d1a22995745e589efc43ea4739111e78
xinleipan <xinleipan@berkeley.edu>
2018-05-28 20:46:07 -0700

    Update train_cont_policy.py

--- mpc_final/train_cont_policy.py
+++ mpc_final/train_cont_policy.py
@@ -220,7 +220,7 @@ def train_policy(args, env, num_steps=40000000):
         if done:
             done_cnt += 1
             obs, prev_info = env.reset()
-            obs, reward, _, info = env.step(np.array([1.0, 0.0]))
+            obs, _, _, info = env.step(np.array([1.0, 0.0]))
             buffer_manager.reset(prev_info, tt)
             action_manager.reset()
         
