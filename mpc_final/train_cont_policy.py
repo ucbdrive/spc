5a0cfced4647632a8f0f6eaed8ca867ecaf22a6a
xinleipan <xinleipan@berkeley.edu>
2018-06-03 19:55:50 +0000

    update train cont policy:

--- mpc_final/train_cont_policy.py
+++ mpc_final/train_cont_policy.py
@@ -173,6 +173,9 @@ class ActionSampleManager:
         if self.args.use_dqn and self.args.continuous:
             if abs(act[1]) <= dqn_act * 0.1:
                 act[1] = 0
+        elif self.args.continuous and self.args.use_dqn == False:
+            if abs(act[1]) <= 0.8:
+                act[1] = 0
         return act, dqn_act        
 
 def train_policy(args, env, num_steps=40000000):
@@ -231,7 +234,7 @@ def train_policy(args, env, num_steps=40000000):
             dqn_agent.store_effect(dqn_action, reward['with_pos'], done)
         
         if tt % args.learning_freq == 0 and tt > args.learning_starts and buffer_manager.mpc_buffer.can_sample(args.batch_size):
-            for ep in range(30):
+            for ep in range(10):
                 optimizer.zero_grad()
                 loss = train_model(args, train_net, buffer_manager.mpc_buffer, epoch, buffer_manager.avg_img, buffer_manager.std_img)
                 print('loss = %0.4f\n' % loss.data.cpu().numpy())
