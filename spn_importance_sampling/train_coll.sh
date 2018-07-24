3f069bc63fe153afb35bae83e3e5825335ea13dc
Xiangyu Chen <1023764859@qq.com>
2018-07-24 10:26:34 +0800

    importance sampling

---
+++ spn_importance_sampling/train_coll.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+python3 train_torcs.py \
+    --save-path mpc_5_cont_pretrain_coll \
+    --continuous \
+    --num-total-act 2 \
+    --pred-step 5 \
+    --buffer-size 50000 \
+    --epsilon-frames 100000 \
+    --batch-size 32 \
+    --use-offroad \
+    --use-collision \
+    --use-angle \
+    --use-distance \
+    --sample-with-angle \
+    --sample-with-offroad \
+    --sample-with-collision \
+    --sample-with-distance \
+    --num-same-step 1 \
+    --id 25 \
+    --resume
